a115894dd035180223def54a65be5fd208a47eaa
Daniel Wille <daniel.wille@axiosengineering.com>
2015-09-01 11:18:04 -0400

    Update class names

--- graphical/gov.redhawk.ide.graphiti.sad.ui.tests/src/gov/redhawk/ide/graphiti/sad/ui/tests/PortStatsPrefPageTest.java
+++ graphical/gov.redhawk.ide.graphiti.sad.ui.tests/src/gov/redhawk/ide/graphiti/sad/ui/tests/PortStatsPrefPageTest.java
@@ -10,12 +10,6 @@
  *******************************************************************************/
 package gov.redhawk.ide.graphiti.sad.ui.tests;
 
-import gov.redhawk.ide.graphiti.sad.ui.SADUIGraphitiPlugin;
-import gov.redhawk.ide.graphiti.sad.ui.preferences.GraphitiSadMessages;
-import gov.redhawk.ide.graphiti.sad.ui.preferences.GraphitiSadPreferenceConstants;
-import gov.redhawk.ide.graphiti.sad.ui.preferences.PortStatisticsPreferencePage;
-import gov.redhawk.ide.swtbot.diagram.AbstractGraphitiTest;
-
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.swt.widgets.Display;
@@ -26,6 +20,12 @@ import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
 import org.junit.Assert;
 import org.junit.Test;
+
+import gov.redhawk.ide.graphiti.sad.ui.SADUIGraphitiPlugin;
+import gov.redhawk.ide.graphiti.ui.diagram.preferences.DiagramPreferenceConstants;
+import gov.redhawk.ide.graphiti.ui.diagram.preferences.GraphitiPreferencesNLS;
+import gov.redhawk.ide.graphiti.ui.diagram.preferences.PortStatisticsPreferencePage;
+import gov.redhawk.ide.swtbot.diagram.AbstractGraphitiTest;
 
 public class PortStatsPrefPageTest extends AbstractGraphitiTest {
 
@@ -53,26 +53,26 @@ public class PortStatsPrefPageTest extends AbstractGraphitiTest {
 		Assert.assertTrue(okButton.isEnabled());
 
 		// TEST INVALID INPUT
-		SWTBotText queueLevelText = prefBot.textWithLabel(GraphitiSadMessages.PortStatPreference_warningQueueLevel);
+		SWTBotText queueLevelText = prefBot.textWithLabel(GraphitiPreferencesNLS.PortStatPreference_warningQueueLevel);
 		queueLevelText.typeText("NotANumber");
-		Assert.assertTrue("Error label not displayed as expected.  Expected: " + GraphitiSadMessages.PortStatPreference_warningQueueLevelError
-			+ " but displayed: " + getErrorMessage(prefShell), GraphitiSadMessages.PortStatPreference_warningQueueLevelError.equals(getErrorMessage(prefShell)));
+		Assert.assertTrue("Error label not displayed as expected.  Expected: " + GraphitiPreferencesNLS.PortStatPreference_warningQueueLevelError
+			+ " but displayed: " + getErrorMessage(prefShell), GraphitiPreferencesNLS.PortStatPreference_warningQueueLevelError.equals(getErrorMessage(prefShell)));
 		Assert.assertTrue(!applyButton.isEnabled());
 		Assert.assertTrue(defaultButton.isEnabled());
 		Assert.assertTrue(!okButton.isEnabled());
 
-		SWTBotText timeSinceLastPushText = prefBot.textWithLabel(GraphitiSadMessages.PortStatPreference_warningNoData);
+		SWTBotText timeSinceLastPushText = prefBot.textWithLabel(GraphitiPreferencesNLS.PortStatPreference_warningNoData);
 		timeSinceLastPushText.typeText("NotANumber");
-		Assert.assertTrue("Error label not displayed as expected.  Expected: " + GraphitiSadMessages.PortStatPreference_warningNoDataError + " but displayed: "
-			+ getErrorMessage(prefShell), GraphitiSadMessages.PortStatPreference_warningNoDataError.equals(getErrorMessage(prefShell)));
+		Assert.assertTrue("Error label not displayed as expected.  Expected: " + GraphitiPreferencesNLS.PortStatPreference_warningNoDataError + " but displayed: "
+			+ getErrorMessage(prefShell), GraphitiPreferencesNLS.PortStatPreference_warningNoDataError.equals(getErrorMessage(prefShell)));
 		Assert.assertTrue(!applyButton.isEnabled());
 		Assert.assertTrue(defaultButton.isEnabled());
 		Assert.assertTrue(!okButton.isEnabled());
 
-		SWTBotText queueFlushText = prefBot.textWithLabel(GraphitiSadMessages.PortStatPreference_errorQueueFlush);
+		SWTBotText queueFlushText = prefBot.textWithLabel(GraphitiPreferencesNLS.PortStatPreference_errorQueueFlush);
 		queueFlushText.typeText("NotANumber");
-		Assert.assertTrue("Error label not displayed as expected.  Expected: " + GraphitiSadMessages.PortStatPreference_errorQueueFlushError
-			+ " but displayed: " + getErrorMessage(prefShell), GraphitiSadMessages.PortStatPreference_errorQueueFlushError.equals(getErrorMessage(prefShell)));
+		Assert.assertTrue("Error label not displayed as expected.  Expected: " + GraphitiPreferencesNLS.PortStatPreference_errorQueueFlushError
+			+ " but displayed: " + getErrorMessage(prefShell), GraphitiPreferencesNLS.PortStatPreference_errorQueueFlushError.equals(getErrorMessage(prefShell)));
 		Assert.assertTrue(!applyButton.isEnabled());
 		Assert.assertTrue(defaultButton.isEnabled());
 		Assert.assertTrue(!okButton.isEnabled());
@@ -82,12 +82,12 @@ public class PortStatsPrefPageTest extends AbstractGraphitiTest {
 		IPreferenceStore store = SADUIGraphitiPlugin.getDefault().getPreferenceStore();
 		Assert.assertNull("No error message should be present", getErrorMessage(prefShell));
 		Assert.assertTrue("Default value not reset for " + queueLevelText,
-			store.getDefaultDouble(GraphitiSadPreferenceConstants.PREF_SAD_PORT_STATISTICS_QUEUE_LEVEL) == Double.valueOf(queueLevelText.getText()));
+			store.getDefaultDouble(DiagramPreferenceConstants.PREF_PORT_STATISTICS_QUEUE_LEVEL) == Double.valueOf(queueLevelText.getText()));
 		Assert.assertTrue(
 			"Default value not reset for " + timeSinceLastPushText,
-			store.getDefaultDouble(GraphitiSadPreferenceConstants.PREF_SAD_PORT_STATISTICS_NO_DATA_PUSHED_SECONDS) == Double.valueOf(timeSinceLastPushText.getText()));
+			store.getDefaultDouble(DiagramPreferenceConstants.PREF_PORT_STATISTICS_NO_DATA_PUSHED_SECONDS) == Double.valueOf(timeSinceLastPushText.getText()));
 		Assert.assertTrue("Default value not reset for " + queueFlushText,
-			store.getDefaultDouble(GraphitiSadPreferenceConstants.PREF_SAD_PORT_STATISTICS_QUEUE_FLUSH_DISPLAY) == Double.valueOf(queueFlushText.getText()));
+			store.getDefaultDouble(DiagramPreferenceConstants.PREF_PORT_STATISTICS_QUEUE_FLUSH_DISPLAY) == Double.valueOf(queueFlushText.getText()));
 
 		Assert.assertTrue(applyButton.isEnabled());
 		Assert.assertTrue(defaultButton.isEnabled());
@@ -107,9 +107,9 @@ public class PortStatsPrefPageTest extends AbstractGraphitiTest {
 		bot.menu("Window").menu("Preferences").click();
 		prefShell = bot.shell("Preferences");
 		prefBot = prefShell.bot();
-		queueLevelText = prefBot.textWithLabel(GraphitiSadMessages.PortStatPreference_warningQueueLevel);
-		timeSinceLastPushText = prefBot.textWithLabel(GraphitiSadMessages.PortStatPreference_warningNoData);
-		queueFlushText = prefBot.textWithLabel(GraphitiSadMessages.PortStatPreference_errorQueueFlush);
+		queueLevelText = prefBot.textWithLabel(GraphitiPreferencesNLS.PortStatPreference_warningQueueLevel);
+		timeSinceLastPushText = prefBot.textWithLabel(GraphitiPreferencesNLS.PortStatPreference_warningNoData);
+		queueFlushText = prefBot.textWithLabel(GraphitiPreferencesNLS.PortStatPreference_errorQueueFlush);
 		Assert.assertTrue("Input did not persist for " + queueLevelText, 10.0 == Double.valueOf(queueLevelText.getText()));
 		Assert.assertTrue("Input did not persist for " + timeSinceLastPushText, 10.0 == Double.valueOf(timeSinceLastPushText.getText()));
 		Assert.assertTrue("Input did not persist for " + queueFlushText, 10.0 == Double.valueOf(queueFlushText.getText()));
