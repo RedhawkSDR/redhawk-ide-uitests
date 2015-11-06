f66a90c33572f76b51976fd9f0764b790734c68b
Daniel Wille <daniel.wille@axiosengineering.com>
2015-11-06 11:34:25 -0500

    Attempt to correct issues getting a tree item when a deferred content adapter is involved

--- graphical/gov.redhawk.ide.ui.tests.prf/src/gov/redhawk/ide/ui/tests/prf/ImportPropertyTest.java
+++ graphical/gov.redhawk.ide.ui.tests.prf/src/gov/redhawk/ide/ui/tests/prf/ImportPropertyTest.java
@@ -10,20 +10,28 @@
  */
 package gov.redhawk.ide.ui.tests.prf;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.Assert;
 import org.junit.Test;
+
+import gov.redhawk.ide.swtbot.StandardTestActions;
 
 public class ImportPropertyTest extends AbstractPropertyTabTest {
 
 	@Test
 	public void testAddPropertyFromBrowse() {
 		bot.button("Browse...").click();
-		bot.text().setText("frequency");
-		bot.tree().getTreeItem("Target SDR").select();
-		bot.tree().getTreeItem("Target SDR").getNode("Components").getNode("rh.SigGen").getNode("frequency").select();
-		bot.button("Finish").click();
+		SWTBot dialogBot = bot.shell("Browse Properties").bot();
+
+		dialogBot.text().setText("frequency");
+		List<String> path = Arrays.asList("Target SDR", "Components", "rh.SigGen", "frequency");
+		StandardTestActions.waitForTreeItemToAppear(dialogBot, dialogBot.tree(), path).select();
+		dialogBot.button("Finish").click();
+
 		assertFormValid();
 		editor.bot().tree().getTreeItem("frequency").select();
 		Assert.assertEquals("double (64-bit)", bot.comboBoxWithLabel("Type*:").getText());
@@ -33,17 +41,16 @@ public class ImportPropertyTest extends AbstractPropertyTabTest {
 	public void testBug1295_BrowseWizardValidation() {
 		bot.button("Browse...").click();
 		SWTBot dialogBot = bot.shell("Browse Properties").bot();
-		
-		SWTBotTreeItem item = dialogBot.tree().getTreeItem("Target SDR").select();
-		Assert.assertFalse("Finish button should not be enabled.", dialogBot.button("Finish").isEnabled());
 
-		item = item.expand().getNode("Components").select();
-		Assert.assertFalse("Finish button should not be enabled.", dialogBot.button("Finish").isEnabled());
+		List<String> path = Arrays.asList("Target SDR", "Components", "rh.SigGen", "frequency");
+		for (int i = 1; i < path.size(); i++) {
+			SWTBotTreeItem treeItem = StandardTestActions.waitForTreeItemToAppear(dialogBot, dialogBot.tree(), path.subList(0, i));
+			treeItem.select();
+			Assert.assertFalse("Finish button should not be enabled.", dialogBot.button("Finish").isEnabled());
+		}
 
-		item = item.expand().getNode("rh.SigGen").select();
-		Assert.assertFalse("Finish button should not be enabled.", dialogBot.button("Finish").isEnabled());
-
-		item = item.expand().getNode("frequency").select();
+		SWTBotTreeItem treeItem = StandardTestActions.waitForTreeItemToAppear(dialogBot, dialogBot.tree(), path);
+		treeItem.select();
 		Assert.assertTrue("Finish button should be enabled.", dialogBot.button("Finish").isEnabled());
 
 		dialogBot.button("Cancel").click();
