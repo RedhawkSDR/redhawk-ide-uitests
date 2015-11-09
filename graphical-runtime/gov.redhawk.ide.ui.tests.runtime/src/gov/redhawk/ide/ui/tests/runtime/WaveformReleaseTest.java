abc8b3da999c1d9d23d9d2c36775978814a9585c
Daniel Wille <daniel.wille@axiosengineering.com>
2015-11-09 16:06:50 -0500

    Remove redundant code

--- graphical-runtime/gov.redhawk.ide.ui.tests.runtime/src/gov/redhawk/ide/ui/tests/runtime/WaveformReleaseTest.java
+++ graphical-runtime/gov.redhawk.ide.ui.tests.runtime/src/gov/redhawk/ide/ui/tests/runtime/WaveformReleaseTest.java
@@ -11,71 +11,34 @@
  */
 package gov.redhawk.ide.ui.tests.runtime;
 
-import gov.redhawk.ide.swtbot.ComponentUtils;
-import gov.redhawk.ide.swtbot.UIRuntimeTest;
-import gov.redhawk.ide.swtbot.WaveformUtils;
-
-import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
-import org.eclipse.swtbot.swt.finder.SWTBot;
-import org.eclipse.swtbot.swt.finder.waits.ICondition;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.Test;
 
+import gov.redhawk.ide.swtbot.UIRuntimeTest;
+import gov.redhawk.ide.swtbot.WaveformUtils;
+import gov.redhawk.ide.swtbot.scaExplorer.ScaExplorerTestUtils;
+
 public class WaveformReleaseTest extends UIRuntimeTest {
-	
-	SWTBot viewBot;
-	
-	@Override
-	public void before() throws Exception {
-		super.before();
-		
-		SWTBotView explorerView = bot.viewById("gov.redhawk.ui.sca_explorer");
-		explorerView.show();
-		explorerView.setFocus();
-		viewBot = explorerView.bot();
-	}
-	
+
 	/**
 	 * IDE-913 - Ensure waveform release action completes successfully
 	 */
 	@Test
 	public void releaseWaveformTest() {
 		final String WAVEFORM = "ExampleWaveform01";
-		final String COMPONENT = "SigGen";
+		final String COMPONENT = "rh.SigGen";
+		final String COMPONENT_1 = "SigGen_1";
 		final String COMPONENT_IMPL = "python";
 		final String COMPONENT_PORT = "dataFloat_out";
-		
+
 		SWTBotTreeItem waveformTreeItem = WaveformUtils.launchLocalWaveform(bot, WAVEFORM);
-		SWTBotTreeItem componentTreeItem = ComponentUtils.launchLocalComponent(bot, COMPONENT, COMPONENT_IMPL);
-	
+		ScaExplorerTestUtils.launchComponentFromTargetSDR(bot, COMPONENT, COMPONENT_IMPL);
+		SWTBotTreeItem componentTreeItem = ScaExplorerTestUtils.waitUntilNodeAppearsInScaExplorer(bot, new String[] { "Sandbox", "Chalkboard" }, COMPONENT_1);
+
 		// Release waveform
 		waveformTreeItem.contextMenu("Release").click();
-		
-		// Make sure waveform is removed from the REDHAWK Explorer
-		final SWTBotTreeItem sandbox = viewBot.tree().getTreeItem("Sandbox");
-		bot.waitUntil(new ICondition() {
-
-			@Override
-			public boolean test() throws Exception {
-				for (SWTBotTreeItem item : sandbox.getItems()) {
-					if (item.getText().matches(WAVEFORM + ".*")) {
-						return false;
-					}
-				}
-
-				return true;
-			}
-
-			@Override
-			public void init(SWTBot bot) {
-			}
-
-			@Override
-			public String getFailureMessage() {
-				return "Waveform: " + WAVEFORM + " did not release";
-			}
-		});
+		ScaExplorerTestUtils.waitUntilNodeRemovedFromScaExplorer(bot, new String[] { "Sandbox" }, WAVEFORM);
 
 		// Make sure waveform is not present in connection wizard
 		componentTreeItem.expand().getNode(COMPONENT_PORT).contextMenu("Connect").click();
