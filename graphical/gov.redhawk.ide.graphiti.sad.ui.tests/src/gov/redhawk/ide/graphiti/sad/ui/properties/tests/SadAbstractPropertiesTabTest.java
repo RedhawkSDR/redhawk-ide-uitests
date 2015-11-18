a2b4399b08dc82ce458bb179ad2dd415b170d39d
Daniel Wille <daniel.wille@axiosengineering.com>
2015-11-18 18:28:06 -0500

    Add tests for the properties tab in the SAD editor

---
+++ graphical/gov.redhawk.ide.graphiti.sad.ui.tests/src/gov/redhawk/ide/graphiti/sad/ui/properties/tests/SadAbstractPropertiesTabTest.java
@@ -0,0 +1,128 @@
+/**
+ * This file is protected by Copyright.
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package gov.redhawk.ide.graphiti.sad.ui.properties.tests;
+
+import java.util.List;
+
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import gov.redhawk.ide.swtbot.StandardTestActions;
+import gov.redhawk.ide.swtbot.UITest;
+import gov.redhawk.ide.swtbot.WaveformUtils;
+import gov.redhawk.ide.swtbot.diagram.DiagramTestUtils;
+import gov.redhawk.ide.swtbot.diagram.RHBotGefEditor;
+import gov.redhawk.ide.swtbot.diagram.RHSWTGefBot;
+
+public abstract class SadAbstractPropertiesTabTest extends UITest {
+
+	private static final String SIG_GEN = "rh.SigGen";
+	protected static final String SIG_GEN_1 = "SigGen_1";
+	private static final String DATA_CONVERTER = "rh.DataConverter";
+	protected static final String DATA_CONVERTER_1 = "DataConverter_1";
+
+	private static final int COLUMN_ID = 0;
+	private static final int COLUMN_VALUE = 3;
+
+	private SWTBot editorBot;
+
+	@Before
+	public void before() throws Exception {
+		super.before();
+
+		WaveformUtils.createNewWaveform(bot, "SadAbstractPropertiesTabTest", SIG_GEN);
+		RHBotGefEditor editor = new RHSWTGefBot().rhGefEditor("SadAbstractPropertiesTabTest");
+		DiagramTestUtils.addFromPaletteToDiagram(editor, DATA_CONVERTER, 150, 0);
+
+		editorBot = editor.bot();
+		editorBot.cTabItem("Properties").activate();
+	}
+
+	@Test
+	public void componentsPresent() {
+		SWTBotTreeItem[] topLevelItems = editorBot.tree().getAllItems();
+		Assert.assertEquals(2, topLevelItems.length);
+		Assert.assertEquals(SIG_GEN_1, topLevelItems[0].cell(COLUMN_ID));
+		Assert.assertEquals(DATA_CONVERTER_1, topLevelItems[1].cell(COLUMN_ID));
+	}
+
+	protected abstract List<String> getBooleanPath();
+
+	@Test
+	public void setBoolean() {
+		List<String> path = getBooleanPath();
+		SWTBotTreeItem treeItem = StandardTestActions.waitForTreeItemToAppear(editorBot, editorBot.tree(), path);
+		setPropertyEnum(treeItem, "true");
+		setPropertyEnum(treeItem, "false");
+		setPropertyEnum(treeItem, "");
+	}
+
+	protected abstract List<String> getDoublePath();
+
+	@Test
+	public void setDouble() {
+		List<String> path = getDoublePath();
+		SWTBotTreeItem treeItem = StandardTestActions.waitForTreeItemToAppear(editorBot, editorBot.tree(), path);
+		setPropertyValue(treeItem, "abc", "");
+		setPropertyValue(treeItem, "123", "123");
+		setPropertyValue(treeItem, "4.56", "4.56");
+		setPropertyValue(treeItem, "-7.8e9", "-7.8e9");
+	}
+
+	protected abstract List<String> getEnumPath();
+
+	@Test
+	public void setEnum() {
+		List<String> path = getEnumPath();
+		SWTBotTreeItem treeItem = StandardTestActions.waitForTreeItemToAppear(editorBot, editorBot.tree(), path);
+		setPropertyEnum(treeItem, "Complex");
+		setPropertyEnum(treeItem, "");
+	}
+
+	protected abstract List<String> getLongPath();
+
+	@Test
+	public void setLong() {
+		List<String> path = getLongPath();
+		SWTBotTreeItem treeItem = StandardTestActions.waitForTreeItemToAppear(editorBot, editorBot.tree(), path);
+		setPropertyValue(treeItem, "abc", "");
+		setPropertyValue(treeItem, "123", "123");
+		setPropertyValue(treeItem, "4.56", "123");
+		setPropertyValue(treeItem, "-7.8e9", "123");
+	}
+
+	protected abstract List<String> getStringPath();
+
+	@Test
+	public void setString() {
+		List<String> path = getStringPath();
+		SWTBotTreeItem treeItem = StandardTestActions.waitForTreeItemToAppear(editorBot, editorBot.tree(), path);
+		setPropertyValue(treeItem, "abc", "abc");
+		setPropertyValue(treeItem, "123", "123");
+	}
+
+	private void setPropertyValue(SWTBotTreeItem propertyItem, String typeText, String expectedCellValue) {
+		StandardTestActions.writeToCell(editorBot, propertyItem, COLUMN_VALUE, typeText);
+		String msg = String.format("Property '%s' set to wrong value", propertyItem.cell(COLUMN_ID));
+		Assert.assertEquals(msg, expectedCellValue, propertyItem.cell(COLUMN_VALUE));
+	}
+
+	private void setPropertyEnum(SWTBotTreeItem propertyItem, String selectText) {
+		propertyItem.select();
+		propertyItem.click(COLUMN_VALUE);
+		editorBot.list().select(selectText);
+		String msg = String.format("Property '%s' set to wrong value", propertyItem.cell(COLUMN_ID));
+		Assert.assertEquals(msg, selectText, propertyItem.cell(COLUMN_VALUE));
+	}
+}
---
+++ graphical/gov.redhawk.ide.graphiti.sad.ui.tests/src/gov/redhawk/ide/graphiti/sad/ui/properties/tests/SadPropertiesTabSimpleTest.java
@@ -0,0 +1,49 @@
+/**
+ * This file is protected by Copyright.
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package gov.redhawk.ide.graphiti.sad.ui.properties.tests;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SadPropertiesTabSimpleTest extends SadAbstractPropertiesTabTest {
+
+	private static final String SIG_GEN_DOUBLE = "frequency";
+	private static final String SIG_GEN_STRING = "stream_id";
+	private static final String SIG_GEN_BOOLEAN = "throttle";
+	private static final String SIG_GEN_LONG = "xfer_len";
+	private static final String DATA_CONVERTER_ENUM = "outputType";
+
+	@Override
+	protected List<String> getBooleanPath() {
+		return Arrays.asList(SIG_GEN_1, SIG_GEN_BOOLEAN);
+	}
+
+	@Override
+	protected List<String> getDoublePath() {
+		return Arrays.asList(SIG_GEN_1, SIG_GEN_DOUBLE);
+	}
+
+	@Override
+	protected List<String> getEnumPath() {
+		return Arrays.asList(DATA_CONVERTER_1, DATA_CONVERTER_ENUM);
+	}
+
+	@Override
+	protected List<String> getLongPath() {
+		return Arrays.asList(SIG_GEN_1, SIG_GEN_LONG);
+	}
+
+	@Override
+	protected List<String> getStringPath() {
+		return Arrays.asList(SIG_GEN_1, SIG_GEN_STRING);
+	}
+
+}
