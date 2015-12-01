287067274877584ed62c844677dfd7c92fe1ff9c
Daniel Wille <daniel.wille@axiosengineering.com>
2015-12-01 10:08:19 -0500

    Relocate a few tests

--- graphical/gov.redhawk.ide.graphiti.sad.ui.tests/src/gov/redhawk/ide/graphiti/sad/ui/tests/properties/SadPropertiesTabSimpleTest.java
+++ graphical/gov.redhawk.ide.graphiti.sad.ui.tests/src/gov/redhawk/ide/graphiti/sad/ui/tests/properties/SadPropertiesTabSimpleTest.java
@@ -8,7 +8,7 @@
  * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package gov.redhawk.ide.graphiti.sad.ui.properties.tests;
+package gov.redhawk.ide.graphiti.sad.ui.tests.properties;
 
 import java.util.Arrays;
 import java.util.List;
