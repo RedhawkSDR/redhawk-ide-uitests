d902a283c10a1841c27c2aeb1fa5c58561f91f8c
Daniel Wille <daniel.wille@axiosengineering.com>
2015-12-03 15:35:29 -0500

    Expand general new project wizard tests

--- graphical/gov.redhawk.ide.ui.tests.projectCreation/src/gov/redhawk/ide/ui/tests/projectCreation/GeneralTest.java
+++ graphical/gov.redhawk.ide.ui.tests.projectCreation/src/gov/redhawk/ide/ui/tests/projectCreation/GeneralTest.java
@@ -10,57 +10,83 @@
  *******************************************************************************/
 package gov.redhawk.ide.ui.tests.projectCreation;
 
+import gov.redhawk.ide.swtbot.StandardTestActions;
 import gov.redhawk.ide.swtbot.UITest;
 
+import java.util.Arrays;
+
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class GeneralTest extends UITest {
-	
-	private void testNewProjectWizardAccess(String menuItem, String shellTitle) {
-		
-		SWTBotShell wizardShell;
 
-		// Check SCA Category
-		bot.menu("File").menu("New").menu("Project...").click();
-		wizardShell = bot.shell("New Project");
+	private void testNewProjectWizardAccess(String menuItem, String shellTitle, boolean shortcut) {
+		if (shortcut) {
+			bot.menu().menu("File", "New", menuItem).click();
+			SWTBotShell wizardShell = bot.shell(shellTitle);
+			wizardShell.close();
+		}
+
+		bot.menu().menu("File", "New", "Project...").click();
+		SWTBotShell wizardShell = bot.shell("New Project");
 		Assert.assertTrue(wizardShell.isActive());
-		wizardShell.bot().tree().getTreeItem("REDHAWK").expand().getNode(menuItem).select();
+		SWTBotTreeItem treeItem = StandardTestActions.waitForTreeItemToAppear(wizardShell.bot(), wizardShell.bot().tree(), Arrays.asList("REDHAWK", menuItem));
+		treeItem.select();
 		wizardShell.bot().button("Next >").click();
 		wizardShell = bot.shell(shellTitle);
 		Assert.assertTrue(wizardShell.isActive());
 		wizardShell.close();
 	}
-	
+
 	@Test
-	public void testNewComponentProject() {
-		testNewProjectWizardAccess("REDHAWK Component Project", "New Component Project");
+	public void newComponentProject() {
+		testNewProjectWizardAccess("REDHAWK Component Project", "New Component Project", true);
+	}
+
+	@Test
+	public void newWaveformProject() {
+		testNewProjectWizardAccess("REDHAWK Waveform Project", "New Waveform Project", true);
+	}
+
+	@Test
+	public void newDeviceProject() {
+		testNewProjectWizardAccess("REDHAWK Device Project", "New Device Project", true);
+	}
+
+	@Test
+	public void newServiceProject() {
+		testNewProjectWizardAccess("REDHAWK Service Project", "New Service Project", true);
+	}
+
+	@Test
+	public void newNodeProject() {
+		testNewProjectWizardAccess("REDHAWK Node Project", "Node Project", true);
+	}
+
+	@Test
+	public void newControlPanelProject() {
+		testNewProjectWizardAccess("REDHAWK Control Panel Project", "New Plug-in Project", false);
+	}
+
+	@Test
+	public void newFrontEndDeviceProject() {
+		testNewProjectWizardAccess("REDHAWK Front End Device Project", "New Device Project", false);
+	}
+
+	@Test
+	public void newIdlProject() {
+		testNewProjectWizardAccess("REDHAWK IDL Project", "IDL Project", false);
 	}
 	
 	@Test
-	public void testNewWaveformProject() {
-		testNewProjectWizardAccess("REDHAWK Waveform Project", "New Waveform Project");
+	public void newOctaveProject() {
+		testNewProjectWizardAccess("REDHAWK Octave Project", "New Component Project", false);
 	}
 	
 	@Test
-	public void testNewDeviceProject() {
-		testNewProjectWizardAccess("REDHAWK Device Project", "New Device Project");
+	public void newSharedLibraryProject() {
+		testNewProjectWizardAccess("REDHAWK Shared Library Project", "New Shared Library Project", false);
 	}
-	
-	@Test
-	public void testNewServiceProject() {
-		testNewProjectWizardAccess("REDHAWK Service Project", "New Service Project");
-	}
-	
-	@Test
-	public void testNewNodeProject() {
-		testNewProjectWizardAccess("REDHAWK Node Project", "Node Project");
-	}
-	
-	@Test
-	public void testNewControlPanelProject() {
-		testNewProjectWizardAccess("REDHAWK Control Panel Project", "New Plug-in Project");
-	}
-	
 }
