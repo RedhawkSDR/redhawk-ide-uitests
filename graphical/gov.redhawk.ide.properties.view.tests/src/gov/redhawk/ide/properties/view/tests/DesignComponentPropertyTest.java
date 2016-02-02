9d7360587615e710c3da822c87fecedd0e233d41
Devin Nelson <devin.nelson@axiosengineering.com>
2016-02-02 11:19:02 -0500

    Test for IDE-1338

--- graphical/gov.redhawk.ide.properties.view.tests/src/gov/redhawk/ide/properties/view/tests/DesignComponentPropertyTest.java
+++ graphical/gov.redhawk.ide.properties.view.tests/src/gov/redhawk/ide/properties/view/tests/DesignComponentPropertyTest.java
@@ -7,7 +7,14 @@ import java.io.IOException;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.Assert;
+import org.junit.Test;
 
+import gov.redhawk.ide.swtbot.ProjectExplorerUtils;
+import gov.redhawk.ide.swtbot.ViewUtils;
 import gov.redhawk.ide.swtbot.WaveformUtils;
 import mil.jpeojtrs.sca.partitioning.ComponentProperties;
 import mil.jpeojtrs.sca.partitioning.PartitioningFactory;
@@ -66,5 +73,39 @@ public class DesignComponentPropertyTest extends AbstractPropertiesViewDesignTes
 		sad.getAllComponentInstantiations().get(0).setComponentProperties(componentProps);
 		sad.eResource().save(outputStream, null);
 		editor.toTextEditor().setText(outputStream.toString());
+	}
+
+	// ### Class Specific Tests ### //
+	/**
+	 * Open a waveform project so that the initial tab is not the diagram tab. Ensure that the properties view still
+	 * works.
+	 * 
+	 * IDE-1338
+	 */
+	@Test
+	public void overviewTabDefaultTest() {
+		prepareObject();
+		editor.bot().cTabItem("Overview").activate();
+		editor.close();
+
+		ProjectExplorerUtils.openProjectInEditor(bot, WAVEFORM_NAME, WAVEFORM_NAME + ".sad.xml");
+
+		// Need this because 'editor' expects to be a Graphiti Diagram
+		synchronized (bot) {
+			try {
+				bot.wait(500);
+			} catch (InterruptedException e) {
+				// PASS
+			}
+		}
+		SWTBotEditor tmpEditor = bot.activeEditor();
+		tmpEditor.bot().cTabItem("Diagram").activate();
+
+		setEditor();
+		selectObject();
+		SWTBotTree propTree = ViewUtils.selectPropertiesTab(bot, PROP_TAB_NAME);
+		Assert.assertNotNull("Property window does not populate", propTree);
+		SWTBotTreeItem[] items = propTree.getAllItems();
+		Assert.assertTrue("No property values are displayed", items.length > 0);
 	}
 }
