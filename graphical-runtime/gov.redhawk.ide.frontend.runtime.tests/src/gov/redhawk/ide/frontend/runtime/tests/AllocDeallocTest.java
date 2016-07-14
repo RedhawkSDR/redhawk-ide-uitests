bf149304c756433240bf94ceb9bdf58126c189ca
Daniel Wille <daniel.wille@axiosengineering.com>
2016-07-14 17:11:27 -0400

    Rename test package
    In support of IDE-1357

--- graphical-runtime/gov.redhawk.ide.frontend.runtime.tests/src/gov/redhawk/ide/frontend/runtime/tests/AllocDeallocTest.java
+++ graphical-runtime/gov.redhawk.ide.frontend.runtime.tests/src/gov/redhawk/ide/frontend/runtime/tests/AllocDeallocTest.java
@@ -8,7 +8,7 @@
  * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package gov.redhawk.ide.frontend.tests;
+package gov.redhawk.ide.frontend.runtime.tests;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.swtbot.swt.finder.SWTBot;
