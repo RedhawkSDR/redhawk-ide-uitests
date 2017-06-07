69edad8187fdc29fed2eada58e06ceb8c537ddb8
Devin Nelson <dnelson2@lgsinnovations.com>
2017-06-07 15:37:56 -0400

    IDE-1520 - Test that IDL section shows in the properties view for ports selected in the REDHAWK explorer

---
+++ graphical-runtime/gov.redhawk.ide.graphiti.sad.ui.runtime.chalkboard.tests/src/gov/redhawk/ide/graphiti/sad/ui/runtime/chalkboard/tests/PortPropertiesTest.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * This file is protected by Copyright. 
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package gov.redhawk.ide.graphiti.sad.ui.runtime.chalkboard.tests;
+
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.Assert;
+import org.junit.Test;
+
+import gov.redhawk.ide.swtbot.ViewUtils;
+import gov.redhawk.ide.swtbot.diagram.RHBotGefEditor;
+import gov.redhawk.ide.swtbot.scaExplorer.ScaExplorerTestUtils;
+
+public class PortPropertiesTest extends AbstractGraphitiChalkboardTest {
+
+	private RHBotGefEditor editor;
+
+	/**
+	 * IDE-1520 - Show IDL tree in port details for explorer view
+	 */
+	@Test
+	public void checkPortProperties() {
+		ScaExplorerTestUtils.launchComponentFromTargetSDR(bot, HARD_LIMIT, "python");
+		ScaExplorerTestUtils.waitUntilNodeAppearsInScaExplorer(bot, CHALKBOARD_PATH, HARD_LIMIT_1);
+		
+		SWTBotTreeItem providesPort = ScaExplorerTestUtils.getTreeItemFromScaExplorer(bot, new String[] {"Sandbox", CHALKBOARD, HARD_LIMIT_1 }, "dataFloat_in");
+		SWTBotTreeItem usesPort = ScaExplorerTestUtils.getTreeItemFromScaExplorer(bot, new String[] {"Sandbox", CHALKBOARD, HARD_LIMIT_1 }, "dataFloat_out");
+		
+		final String inDesc = "Float input port for data before hard limit is applied. ";
+		final String outDesc = "Float output port for data after hard limit is applied. ";
+		
+		checkPortDetails(providesPort, inDesc);
+		checkPortDetails(usesPort, outDesc);
+	}
+
+	private void checkPortDetails(SWTBotTreeItem port, String expectedDesc) {
+		SWTBotView propView = bot.viewById(ViewUtils.PROPERTIES_VIEW_ID);
+		propView.show();
+		port.select();
+		ViewUtils.selectPropertiesTab(bot, "Port Details");
+		String description = propView.bot().textWithLabel("Description:").getText(); // IDE-1172
+		Assert.assertEquals("provides Port description", expectedDesc, description);
+		SWTBotTree tree = gefBot.viewByTitle("Properties").bot().tree();
+		tree.expandNode("dataFloat");
+		Assert.assertTrue("Properties view tree should have multiple nodes", tree.visibleRowCount() > 1);
+		
+	}
+}
--- graphical/gov.redhawk.ide.graphiti.sad.ui.tests/src/gov/redhawk/ide/graphiti/sad/ui/tests/PortPropertiesTest.java
+++ graphical/gov.redhawk.ide.graphiti.sad.ui.tests/src/gov/redhawk/ide/graphiti/sad/ui/tests/PortPropertiesTest.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package gov.redhawk.ide.graphiti.sad.ui.tests;
 
-import org.eclipse.graphiti.mm.pictograms.Anchor;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
@@ -25,8 +24,7 @@ import gov.redhawk.ide.swtbot.diagram.RHBotGefEditor;
 
 public class PortPropertiesTest extends AbstractGraphitiTest {
 
-	public PortPropertiesTest() {
-	}
+	protected static final String HARD_LIMIT = "rh.HardLimit";
 
 	/**
 	 * Ensure that after selecting a port the IDL hierarchy for it is shown in the properties view.
@@ -38,45 +36,27 @@ public class PortPropertiesTest extends AbstractGraphitiTest {
 		WaveformUtils.createNewWaveform(gefBot, waveformName, null);
 		final RHBotGefEditor editor = gefBot.rhGefEditor(waveformName);
 		DiagramTestUtils.addFromPaletteToDiagram(editor, onlyComponent, 0, 0);
-		SWTBotGefEditPart part;
-		SWTBotTree tree;
-		String description;
 
-		part = getAnchorPart(DiagramTestUtils.getDiagramProvidesPort(editor, onlyComponent));
+		SWTBotGefEditPart providesPortEditPart = DiagramTestUtils.getDiagramPortAnchor(DiagramTestUtils.getDiagramProvidesPort(editor, HARD_LIMIT));
+		SWTBotGefEditPart usesPortEditPart = DiagramTestUtils.getDiagramPortAnchor(DiagramTestUtils.getDiagramUsesPort(editor, HARD_LIMIT));
+
+		final String inDesc = "Float input port for data before hard limit is applied. ";
+		final String outDesc = "Float output port for data after hard limit is applied. ";
+
+		checkPortDetails(providesPortEditPart, editor, inDesc);
+		checkPortDetails(usesPortEditPart, editor, outDesc);
+	}
+
+	private void checkPortDetails(SWTBotGefEditPart part, RHBotGefEditor editor, String expectedDesc) {
 		SWTBotView propView = bot.viewById(ViewUtils.PROPERTIES_VIEW_ID);
 		propView.show();
 		editor.select(part);
 		editor.click(part);
 		ViewUtils.selectPropertiesTab(bot, "Port Details");
-		description = propView.bot().textWithLabel("Description:").getText(); // IDE-1172
-		Assert.assertEquals("provides Port description", "Float input port for data before hard limit is applied. ", description);
-		tree = gefBot.viewByTitle("Properties").bot().tree();
+		String description = propView.bot().textWithLabel("Description:").getText(); // IDE-1172
+		Assert.assertEquals("provides Port description", expectedDesc, description);
+		SWTBotTree tree = gefBot.viewByTitle("Properties").bot().tree();
 		tree.expandNode("dataFloat");
 		Assert.assertTrue("Properties view tree should have multiple nodes", tree.visibleRowCount() > 1);
-
-		part = getAnchorPart(DiagramTestUtils.getDiagramUsesPort(editor, onlyComponent));
-		propView.show();
-		editor.select(part);
-		editor.click(part);
-		ViewUtils.selectPropertiesTab(bot, "Port Details");
-		description = gefBot.viewByTitle("Properties").bot().textWithLabel("Description:").getText(); // IDE-1172
-		Assert.assertEquals("uses Port description", "Float output port for data after hard limit is applied. ", description);
-		tree = gefBot.viewByTitle("Properties").bot().tree();
-		tree.expandNode("dataFloat");
-		Assert.assertTrue("Properties view tree should have multiple nodes", tree.visibleRowCount() > 1);
-	}
-
-
-	private SWTBotGefEditPart getAnchorPart(SWTBotGefEditPart parent) {
-		if (parent.part().getModel() instanceof Anchor) {
-			return parent;
-		}
-		for (SWTBotGefEditPart part: parent.children()) {
-			SWTBotGefEditPart partAnchor = getAnchorPart(part);
-			if  (partAnchor != null) {
-				return partAnchor;
-			}
-		}
-		return null;
 	}
 }
