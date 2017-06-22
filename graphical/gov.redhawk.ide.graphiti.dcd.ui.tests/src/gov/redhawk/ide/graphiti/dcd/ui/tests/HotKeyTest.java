2fde234806601521a6c3614db018f5167fb9d2b1
Daniel Wille <DWille@lgsinnovations.com>
2017-06-22 16:26:47 -0400

    IDE-1990 Correct issues with HotKeyTest

--- graphical/gov.redhawk.ide.graphiti.dcd.ui.tests/src/gov/redhawk/ide/graphiti/dcd/ui/tests/HotKeyTest.java
+++ graphical/gov.redhawk.ide.graphiti.dcd.ui.tests/src/gov/redhawk/ide/graphiti/dcd/ui/tests/HotKeyTest.java
@@ -8,22 +8,22 @@
  * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package gov.redhawk.ide.graphiti.sad.ui.tests;
+package gov.redhawk.ide.graphiti.dcd.ui.tests;
 
 import gov.redhawk.ide.graphiti.ui.tests.AbstractHotKeyTest;
 import gov.redhawk.ide.graphiti.ui.tests.ComponentDescription;
-import gov.redhawk.ide.swtbot.WaveformUtils;
+import gov.redhawk.ide.swtbot.NodeUtils;
 
 public class HotKeyTest extends AbstractHotKeyTest {
 
 	@Override
 	protected ComponentDescription getComponent() {
-		return new ComponentDescription("rh.SigGen", null, null);
+		return new ComponentDescription("DeviceStub", null, null);
 	}
 
 	@Override
 	protected String createNewProject(String projectName) {
-		WaveformUtils.createNewWaveform(gefBot, projectName, null);
+		NodeUtils.createNewNodeProject(gefBot, projectName, "REDHAWK_DEV");
 		return projectName;
 	}
 
