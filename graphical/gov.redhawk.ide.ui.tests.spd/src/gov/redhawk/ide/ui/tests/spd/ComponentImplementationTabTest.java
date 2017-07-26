774bebb3520cf20c67591d3b1fe6a9887c34594c
Devin Nelson <dnelson2@lgsinnovations.com>
2017-07-26 17:34:35 -0400

    IDE-2003 - test that all usesdevice properties are visible in the Implementations tab

--- graphical/gov.redhawk.ide.ui.tests.spd/src/gov/redhawk/ide/ui/tests/spd/ComponentImplementationTabTest.java
+++ graphical/gov.redhawk.ide.ui.tests.spd/src/gov/redhawk/ide/ui/tests/spd/ComponentImplementationTabTest.java
@@ -12,6 +12,16 @@ package gov.redhawk.ide.ui.tests.spd;
 
 import java.math.BigInteger;
 
+import org.eclipse.core.runtime.Path;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import gov.redhawk.ide.swtbot.ProjectExplorerUtils;
 import gov.redhawk.ide.swtbot.StandardTestActions;
 import gov.redhawk.ide.swtbot.UITest;
 import gov.redhawk.ide.swtbot.finder.RHBot;
@@ -19,32 +29,27 @@ import gov.redhawk.ide.swtbot.finder.widgets.RHBotSection;
 import gov.redhawk.ui.editor.SCAFormEditor;
 import mil.jpeojtrs.sca.spd.SoftPkg;
 
-import org.eclipse.core.runtime.Path;
-import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
-import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 public class ComponentImplementationTabTest extends UITest {
 
 	private SWTBotEditor editor;
 	private RHBot editorBot;
 	private SoftPkg spd;
 
+	@BeforeClass
+	public static void setupPyDev() throws Exception {
+		StandardTestActions.configurePyDev();
+	}
+
 	@Before
 	public void before() throws Exception {
 		super.before();
 
-		StandardTestActions.importProject(SpdUiTestsActivator.getInstance().getBundle(), new Path("/workspace/CppComTest"), null);
-		// Ensure SPD file was created
-		SWTBotView view = bot.viewById("org.eclipse.ui.navigator.ProjectExplorer");
-		view.show();
-		view.bot().tree().setFocus();
-		view.bot().tree().getTreeItem("CppComTest").select();
-		view.bot().tree().getTreeItem("CppComTest").expand();
-		view.bot().tree().getTreeItem("CppComTest").getNode("CppComTest.spd.xml").doubleClick();
-		
+		final String projectName = "CppComTest";
+
+		StandardTestActions.importProject(SpdUiTestsActivator.getInstance().getBundle(), new Path("/workspace/" + projectName), null);
+
+		// Open SPD file, switch to Implementation tab
+		ProjectExplorerUtils.openProjectInEditor(bot, projectName, projectName + ".spd.xml");
 		editor = bot.editorByTitle("CppComTest");
 		editor.setFocus();
 		editorBot = new RHBot(editor.bot());
@@ -62,7 +67,7 @@ public class ComponentImplementationTabTest extends UITest {
 		editorBot.sleep(600);
 		Assert.assertEquals(spd.getImplementation().get(0).getCode().getEntryPoint(), "someentrypoint");
 	}
-	
+
 	@Test
 	public void testCodePriority() {
 		RHBotSection section = editorBot.section("Code");
@@ -71,7 +76,7 @@ public class ComponentImplementationTabTest extends UITest {
 		editorBot.sleep(600);
 		Assert.assertEquals(spd.getImplementation().get(0).getCode().getPriority(), BigInteger.valueOf(1));
 	}
-	
+
 	@Test
 	public void testCodeLocalFile() {
 		RHBotSection section = editorBot.section("Code");
@@ -80,7 +85,7 @@ public class ComponentImplementationTabTest extends UITest {
 		editorBot.sleep(600);
 		Assert.assertEquals(spd.getImplementation().get(0).getCode().getLocalFile().getName(), "somelocation");
 	}
-	
+
 	@Test
 	public void testCodeStackSize() {
 		RHBotSection section = editorBot.section("Code");
@@ -88,6 +93,39 @@ public class ComponentImplementationTabTest extends UITest {
 		editorBot.textWithLabel("Stack Size:").setText("2");
 		editorBot.sleep(600);
 		Assert.assertEquals(spd.getImplementation().get(0).getCode().getStackSize(), BigInteger.valueOf(2));
+	}
+
+	@Test
+	/**
+	 * Test that all usesdevice properties are visible and display expected labels
+	 */
+	public void testUsesDevice() {
+		SWTBotTree implementationTree = editorBot.tree();
+
+		// Detail all the expected tree nodes
+		String implementation = "cpp *";
+		String usesDevice = "Uses Device usesdevice";
+		String propRef = "propRefID";
+		String simpleRef = "simpleRefID";
+		String simpleSeqRef = "simpleSequenceRefID";
+		String structRef = "structRefID";
+		String structRefSimple = "structSimpleRefID";
+		String structRefSimpleSeq = "structSimpleSequenceRefID";
+		String structSeqRef = "structSequenceRefID";
+		String structValue = "Struct [0]";
+		String structValueSimple = "structValueSimpleRefID";
+		String structValueSimpleSeq = "structValueSimpleSequenceRefID";
+
+		// Check each node (either directly or indirectly as part of a chain to a child node)
+		SWTBotTreeItem implNode = implementationTree.getTreeItem(implementation);
+		implNode.expand().select();
+		implNode.expandNode(usesDevice, propRef).select();
+		implNode.expandNode(usesDevice, simpleRef).select();
+		implNode.expandNode(usesDevice, simpleSeqRef).select();
+		implNode.expandNode(usesDevice, structRef, structRefSimple).select();
+		implNode.expandNode(usesDevice, structRef, structRefSimpleSeq).select();
+		implNode.expandNode(usesDevice, structSeqRef, structValue, structValueSimple).select();
+		implNode.expandNode(usesDevice, structSeqRef, structValue, structValueSimpleSeq).select();
 	}
 
 }
