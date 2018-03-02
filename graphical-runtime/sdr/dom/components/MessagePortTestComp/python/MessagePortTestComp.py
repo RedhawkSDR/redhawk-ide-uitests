dfbd968a3e7af9537b4645be0fe01a5b9a62c377
Daniel Wille <DWille@lgsinnovations.com>
2018-03-02 13:35:47 -0500

    IDE-1007 Add an EventChannel port to test component

--- graphical-runtime/sdr/dom/components/MessagePortTestComp/python/MessagePortTestComp.py
+++ graphical-runtime/sdr/dom/components/MessagePortTestComp/python/MessagePortTestComp.py
@@ -6,27 +6,52 @@
 # Source: MessagePortTestComp.spd.xml
 from ossie.resource import start_component
 import logging
-import time
 
 from MessagePortTestComp_base import *
 
+from ossie import properties
+import CosEventComm__POA
+import time
+
+
 class MessagePortTestComp_i(MessagePortTestComp_base):
-    """<DESCRIPTION GOES HERE>"""
+    """Pushes a message out its ports every two seconds"""
+
+    class Supplier_i(CosEventComm__POA.PushSupplier):
+
+        def disconnect_push_supplier(self):
+            pass
+
     def constructor(self):
-        """
-        This is called by the framework immediately after your component registers with the system.
-        """
-        
+        self.consumer = None
+
     def process(self):
-        msg = MessagePortTestComp_base.Mymessage()
-        msg.EnterMessageHere = "Congrats on your new message!"
-        self.port_message_out.sendMessage(msg)
+        self.mymessage.EnterMessageHere = "Congrats on your new message!"
+        self.port_message_out.sendMessage(self.mymessage)
+
+        # This code only handles 1 connection to the port
+        if len(self.port_eventChannel_out.getConnectionIds()) != 0:
+            connectionID = self.port_eventChannel_out.getConnectionIds()[0]
+            if self.consumer is None:
+                self.consumer = self.port_eventChannel_out.for_suppliers(connectionID).obtain_push_consumer()
+                self.consumer.connect_push_supplier(self.Supplier_i()._this())
+
+            dt = CF.DataType(id=self.mymessage.getId(), value=properties.struct_to_any(self.mymessage))
+            anyProps = properties.props_to_any([dt])
+            try:
+                self.consumer.push(anyProps)
+            except:
+                try:
+                    self.consumer.disconnect_push_consumer()
+                except:
+                    pass
+                self.consumer = None
+
         time.sleep(2)
         return NOOP
 
-  
+
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     logging.debug("Starting Component")
     start_component(MessagePortTestComp_i)
-
--- graphical-runtime/sdr/dom/components/MessagePortTestComp/python/MessagePortTestComp_base.py
+++ graphical-runtime/sdr/dom/components/MessagePortTestComp/python/MessagePortTestComp_base.py
@@ -16,6 +16,9 @@ from ossie.properties import struct_property
 import Queue, copy, time, threading
 from ossie.resource import usesport, providesport, PortCallError
 from ossie.events import MessageSupplierPort
+from ossie.cf import ExtendedCF
+from ossie.cf import ExtendedCF__POA
+import CosEventChannelAdmin
 
 class MessagePortTestComp_base(CF__POA.Resource, Component, ThreadedComponent):
         # These values can be altered in the __init__ of your derived class
@@ -35,6 +38,7 @@ class MessagePortTestComp_base(CF__POA.Resource, Component, ThreadedComponent):
             self.auto_start = False
             # Instantiate the default implementations for all ports on this component
             self.port_message_out = MessageSupplierPort()
+            self.port_eventChannel_out = PortCosEventChannelAdminEventChannelOut_i(self, "eventChannel_out")
 
         def start(self):
             Component.start(self)
@@ -58,9 +62,23 @@ class MessagePortTestComp_base(CF__POA.Resource, Component, ThreadedComponent):
         # DO NOT ADD NEW PORTS HERE.  You can add ports in your derived class, in the SCD xml file, 
         # or via the IDE.
 
+        # 'omg.org/CosEventChannelAdmin/EventChannel' port
+        class PortCosEventChannelAdminEventChannelOut(ExtendedCF__POA.QueryablePort):
+            """This class is a port template for the PortCosEventChannelAdminEventChannelOut_i port and
+            should not be instantiated nor modified.
+            
+            The expectation is that the specific port implementation will extend
+            from this class instead of the base CORBA class ExtendedCF__POA.QueryablePort.
+            """
+            pass
+
         port_message_out = usesport(name="message_out",
                                     repid="IDL:ExtendedEvent/MessageEvent:1.0",
                                     type_="control")
+
+        port_eventChannel_out = usesport(name="eventChannel_out",
+                                         repid="IDL:omg.org/CosEventChannelAdmin/EventChannel:1.0",
+                                         type_="control")
 
         ######################################################################
         # PROPERTIES
@@ -106,5 +124,139 @@ class MessagePortTestComp_base(CF__POA.Resource, Component, ThreadedComponent):
                                     mode="readwrite")
 
 
+
+'''uses port(s)'''
+
+class PortCosEventChannelAdminEventChannelOut_i(MessagePortTestComp_base.PortCosEventChannelAdminEventChannelOut):
+    def __init__(self, parent, name):
+        self.parent = parent
+        self.name = name
+        self.outConnections = {}
+        self.port_lock = threading.Lock()
+
+    def getConnectionIds(self):
+        return self.outConnections.keys()
+
+    def __evaluateRequestBasedOnConnections(self, __connection_id__, returnValue, inOut, out):
+        if __connection_id__=='' and len(self.outConnections) > 1:
+            if (out or inOut or returnValue):
+                raise PortCallError("Returned parameters require either a single connection or a populated __connection_id__ to disambiguate the call.", self.getConnectionIds())
+        if len(self.outConnections) == 0:
+            raise PortCallError("No connections available.",[])
+
+    def connectPort(self, connection, connectionId):
+        self.port_lock.acquire()
+        try:
+            port = connection._narrow(CosEventChannelAdmin.EventChannel)
+            self.outConnections[str(connectionId)] = port
+        finally:
+            self.port_lock.release()
+
+    def disconnectPort(self, connectionId):
+        self.port_lock.acquire()
+        try:
+            self.outConnections.pop(str(connectionId), None)
+        finally:
+            self.port_lock.release()
+
+    def _get_connections(self):
+        self.port_lock.acquire()
+        try:
+            return [ExtendedCF.UsesConnection(name, port) for name, port in self.outConnections.iteritems()]
+        finally:
+            self.port_lock.release()
+
+    def for_consumers(self, __connection_id__ = ""):
+        retVal = None
+        self.port_lock.acquire()
+
+
+        try:
+            self.__evaluateRequestBasedOnConnections(__connection_id__, True, False, False)
+            if __connection_id__:
+                found_connection = False
+                for connId, port in self.outConnections.items():
+                    if __connection_id__ != connId:
+                        continue
+                    found_connection = True
+                    try:
+                        retVal = port.for_consumers()
+                    except Exception:
+                        self.parent._log.exception("The call to for_consumers failed on port %s connection %s instance %s", self.name, connId, port)
+                        raise
+                if not found_connection:
+                    raise PortCallError("Connection id "+__connection_id__+" not found.", self.getConnectionIds())
+            else:
+                for connId, port in self.outConnections.items():
+                    try:
+                        retVal = port.for_consumers()
+                    except Exception:
+                        self.parent._log.exception("The call to for_consumers failed on port %s connection %s instance %s", self.name, connId, port)
+                        raise
+        finally:
+            self.port_lock.release()
+
+        return retVal
+
+    def for_suppliers(self, __connection_id__ = ""):
+        retVal = None
+        self.port_lock.acquire()
+
+
+        try:
+            self.__evaluateRequestBasedOnConnections(__connection_id__, True, False, False)
+            if __connection_id__:
+                found_connection = False
+                for connId, port in self.outConnections.items():
+                    if __connection_id__ != connId:
+                        continue
+                    found_connection = True
+                    try:
+                        retVal = port.for_suppliers()
+                    except Exception:
+                        self.parent._log.exception("The call to for_suppliers failed on port %s connection %s instance %s", self.name, connId, port)
+                        raise
+                if not found_connection:
+                    raise PortCallError("Connection id "+__connection_id__+" not found.", self.getConnectionIds())
+            else:
+                for connId, port in self.outConnections.items():
+                    try:
+                        retVal = port.for_suppliers()
+                    except Exception:
+                        self.parent._log.exception("The call to for_suppliers failed on port %s connection %s instance %s", self.name, connId, port)
+                        raise
+        finally:
+            self.port_lock.release()
+
+        return retVal
+
+    def destroy(self, __connection_id__ = ""):
+        self.port_lock.acquire()
+
+
+        try:
+            self.__evaluateRequestBasedOnConnections(__connection_id__, False, False, False)
+            if __connection_id__:
+                found_connection = False
+                for connId, port in self.outConnections.items():
+                    if __connection_id__ != connId:
+                        continue
+                    found_connection = True
+                    try:
+                        port.destroy()
+                    except Exception:
+                        self.parent._log.exception("The call to destroy failed on port %s connection %s instance %s", self.name, connId, port)
+                        raise
+                if not found_connection:
+                    raise PortCallError("Connection id "+__connection_id__+" not found.", self.getConnectionIds())
+            else:
+                for connId, port in self.outConnections.items():
+                    try:
+                        port.destroy()
+                    except Exception:
+                        self.parent._log.exception("The call to destroy failed on port %s connection %s instance %s", self.name, connId, port)
+                        raise
+        finally:
+            self.port_lock.release()
 
 
