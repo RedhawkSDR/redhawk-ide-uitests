66e88cff1f8b492635848f81aa717b440d8785ff
Daniel Wille <DWille@lgsinnovations.com>
2018-08-15 13:08:27 -0400

    IDE-2200 Relocate test

--- graphical-runtime/gov.redhawk.ide.codegen.runtime.tests/src/gov/redhawk/ide/codegen/runtime/tests/CreateGenerateExportTest.java
+++ graphical-runtime/gov.redhawk.ide.codegen.runtime.tests/src/gov/redhawk/ide/codegen/runtime/tests/CreateGenerateExportTest.java
@@ -8,7 +8,7 @@
  * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html.
  */
-package gov.redhawk.ide.ui.tests.runtime;
+package gov.redhawk.ide.codegen.runtime.tests;
 
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileInfo;
